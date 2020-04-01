b22318665bb6b9ad6b9f20368dbde2b9f3a0f3fb
Samantha Balgobin <sbalgobin94@gmail.com>
2020-04-01 21:53:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,6 +28,7 @@ def won?(board)
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
     return win_combination # return the win_combination indexes that won.
   else
-    false
+    return false
   end
+end
 end
